be02c72ca80c4516718fc765c3269b0f599f1b73
Felix771222 <felixliu1222@gmail.com>
2026-01-09 12:54:56 +0800

    fix: 徹底修復 GitHub Actions 樣式編譯偵測問題

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "index.html",
-    "src/**/*.{vue,js,ts,jsx,tsx}",
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/App.vue",
+    "./src/components/*.vue",
+    "./src/components/**/*.vue",
   ],
   theme: {
     extend: {},
